40ed5df57d65d16d47e91a136db751e88ff6cb8a
JackieYang0627 <jackieyang217@gmail.com>
2023-06-12 01:04:18 -0400

    added delete and insert statements

--- record_company.sql
+++ record_company.sql
@@ -14,3 +14,72 @@ CREATE TABLE albums (
     PRIMARY KEY (id),
     FOREIGN KEY (band_id) REFERENCES bands(id)
 );
+
+INSERT INTO bands (name)
+VALUES ('Iron Maiden');
+
+INSERT INTO bands (name)
+VALUES ('Deuce'), ('Avenged Sevenfold'), ('Ankor');
+
+SELECT * FROM bands; 
+
+SELECT * from bands LIMIT 2;
+
+SELECT name FROM bands;
+
+SELECT id AS 'ID', name as 'BAND NAME'
+FROM bands;
+
+SELECT * FROM bands ORDER BY name DESC;
+
+INSERT INTO albums (name, release_year, band_id)
+VALUES ('The Number of the Beast', 1985, 1),
+	('Power Slave', 1984, 1),
+    ('Nightmare', 2018, 2),
+    ('Nightmare', 2010, 3),
+    ('Test Album', Null , 3);
+    
+SELECT * from albums;
+
+SELECT DISTINCT name FROM albums;
+
+UPDATE albums
+SET release_year = 1982
+WHERE id = 1; 
+
+-- Select years less than 2000 
+SELECT * FROM albums 
+WHERE release_year < 2000;
+
+-- % equates any amounf of letters REGEX
+SELECT * FROM albums 
+WHERE name LIKE '%er%' OR band_id = 2;
+
+SELECT * FROM albums
+WHERE release_year = 1984 AND band_id = 1;
+
+SELECT * FROM albums 
+WHERE release_year BETWEEN 2000 AND 2018;
+
+SELECT * FROM albums 
+WHERE release_year IS NULL;
+
+DELETE FROM albums
+WHERE id = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
